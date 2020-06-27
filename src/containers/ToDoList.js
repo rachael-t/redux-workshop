085516515120dd6292aa9d5dce7585f69bc9d8e7
Rachael Thomas <rnthomas001@gmail.com>
2020-06-27 13:37:30 -0600

    Initial commit and setup for intro to Redux workshop

---
+++ src/containers/ToDoList.js
@@ -0,0 +1,21 @@
+import React from "react";
+import ToDo from "../components/ToDo";
+import { connect } from "react-redux";
+
+const ToDoList = ({ todos }) => {
+  const displayTodos = todos.map((todo) => {
+    return <ToDo {...todo} key={todo.id} />;
+  });
+
+  return (
+    <ul>
+        {displayTodos}
+    </ul>
+  )
+};
+
+const mapStateToProps = (state) => ({
+  todos: state.todos,
+});
+
+export default connect(mapStateToProps)(ToDoList);
